91be0f4769929dbc8c693edeb674a73d9e81f7d1
kirkmans <kirkman@ufl.edu>
2016-10-07 13:54:57 -0400

    First Draft CodePlay We
    Images to Art Web Files Upload.  Used Django

---
+++ formupload/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.htm', {'what':'CodePlay Image to Art Upload'})
+ 
+def upload(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file'], str(request.FILES['file']))
+        return HttpResponse("Successful")
+ 
+    return HttpResponse("Failed")
+ 
+def handle_uploaded_file(file, filename):
+    if not os.path.exists('upload/'):
+        os.mkdir('upload/')
+ 
+    with open('upload/' + filename, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
